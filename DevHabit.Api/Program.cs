4b7c3a6a22791ebb937d71454224e5b0154ad343
Alex <puyaalexxx@gmail.com>
2025-09-17 12:30:04 +0300

    initial setup, opentelemetry setup, directory.build.prop and directory.packages.prop for one place packages across projects

---
+++ DevHabit.Api/Program.cs
@@ -0,0 +1,47 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Add OpenTelemetry
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation())
+    .WithMetrics(metrics => metrics
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation())
+    .UseOtlpExporter();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeScopes = true;
+    options.IncludeFormattedMessage = true;
+});
+//End OpenTelemetry
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
---
+++ README.md
@@ -0,0 +1,12 @@
+# DevHabit - An Asp.Net Core 9 Rest API
+
+## Features:
+
+- Asp.Net Core 9
+- Dependency Injection
+- Docker and docker-compose for containerization
+- Entity Framework Core for data access
+- Repository pattern for data access abstraction
+- Migrations and Data Seed
+- DTos for data transfer
+- Opentelemetry for monitoring and tracing
